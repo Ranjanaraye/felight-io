6d4e07d340fd9fec69f36cecce0977d4e0fd95f0
Ranjanaraye <ranjanachanbatty@gmail.com>
2023-06-11 22:03:50 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './Component/HomePage';
+import CategoryContainer from './Component/categorycontainer/CategoryContainer';
 
 function App() {
   return (
     <div className="App">
-      <h1>good night!</h1>
+      
+      {/*<HomePage />*/}  
+      <CategoryContainer />
+
     </div>
   );
 }
---
+++ src/Component/categorycontainer/CategoryContainer.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import CategoryItem from '../categoryitem/CategoryItem';
+import '../categorycontainer/CategoryContainerStyle.scss'
+
+class CategoryContainer extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            courseCategories: []
+        };
+    }
+    componentDidMount() {
+        fetch('https://ranjanaraye.github.io/course/Course-Category.json')
+          .then(response => response.json())
+          .then(items => this.setState({ courseCategories: items }));
+      }
+
+
+    render() {
+        return (
+            <div className='categories-container'>
+
+                {
+                    /* this.state.courseCategories.map(item => (
+ 
+                         <CategoryItem key={item.id}
+                             title={item.title}
+                             imageUrl={item.imageUrl}
+ 
+                         />
+                     ))*/
+
+
+                    this.state.courseCategories.map(({ id, title, imageUrl }) => (
+                        <CategoryItem key={id}
+                            title={title}
+                            imageUrl={imageUrl} />
+                    ))
+                }
+            </div>
+        )
+    }
+}
+
+export default CategoryContainer;
